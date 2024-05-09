cb1d5427d5ed4782eac2f690ad13d66d44cf6a44
Bryce Kellogg <bryce@kellogg.org>
2024-05-09 14:34:11 -0700

    Improvements to weight graph

--- src/views/WeightView.ts
+++ src/views/WeightView.ts
@@ -44,42 +44,33 @@ export default class WeightView {
      *
      **/
     async handleAddClick(event:MouseEvent) {
-        new class extends Modal {
-            result: string;
-            onSubmit: (result: string) => void;
 
-            constructor(app: App, previousWeight, onSubmit: (result: string) => void) {
+        // ???
+        new class extends Modal {
+
+            // ???
+            constructor(app: App, logFile: TFile) {
                 super(app);
-                this.onSubmit = onSubmit;
-                this.previousWeight = previousWeight;
+                this.logFile = logFile;
+                this.setTitle("Enter Weight");
             }
 
+            // ???
             onOpen() {
-                this.setTitle("Enter Weight");
-                const { contentEl } = this;
-                const input = contentEl.createEl("input", {type: "number", cls: "weight-input", placeholder: this.previousWeight});
-                contentEl.addEventListener("keypress", _ => {
-                    if (_.key == 'Enter') {
-                        const value = contentEl.querySelector('input').value;
+                const input = this.contentEl.createEl("input", {type: "number", cls: "weight-input"});
+                input.focus();
+                input.addEventListener("keypress", ({key, target}) => {
+                    if (key == 'Enter') {
+                        this.app.vault.append(this.logFile, `${DateTime.now().toISO()},${target.value}\n`);
                         this.close();
-                        if (value) this.onSubmit(Number(value).toFixed(1));
                     }
                 });
-                input.focus();
             }
 
-            onClose() {
-                let { contentEl } = this;
-                contentEl.empty();
-            }
-        }(this.app, 186.5, (value) => {
+            // Gets called when we click "x" or press Esc
+            onClose() { this.contentEl.empty() }
 
-            let timestamp = DateTime.now().toISO();
-            let output = `${timestamp},${value}\n`
-
-            // Append new data to end of log file
-            this.app.vault.append(this.logFile, output);
-        }).open();
+        }(this.app, this.logFile).open();
     }
 
 
@@ -88,7 +79,16 @@ export default class WeightView {
      *
      *
      **/
-    async loadData(start, end, groupBy = null) {
+    async loadData() {
+
+        // ???
+        let min = DateTime.fromMillis(this.chart.options.scales.x.min)
+        let max = DateTime.fromMillis(this.chart.options.scales.x.max)
+        const margin = Math.round(max.diff(min).as('days'));
+        const groupBy = 1 + Math.floor(margin / 100);
+
+        
+        const start = min.minus({days: margin});
 
         // Read in log files into strings
         // & split into a list of records
@@ -128,155 +128,173 @@ export default class WeightView {
             value = Number(value);
 
             // TODO: switch on "groupBy" to determine the key
-            const date = timestamp.startOf('day');
-            const key = date.valueOf();
+            // timestamp.ordinal is 1-indexed, so we need to subtract by one
+            // or else dates show up one day later than expected
+            const date = Math.floor(timestamp.ordinal / groupBy)*groupBy - 1;
+            const key = timestamp.startOf('year').plus({days: date}).valueOf();
 
             // If this is the first time seeing this particular
             // key, create a default record that we can update.
             if (acc[key] == undefined) {
                 acc[key] = {
-                    'key': key,
-                    'min': Infinity,
-                    'max': -Infinity,
-                    'mean': 0,
-                    'count': 0,
-                    'values': [],
+                    lastTimestamp: timestamp,
+                    firstTimestamp: timestamp,
+                    lastValue: value,
+                    key: key,
+                    min: value,
+                    max: value,
+                    mean: value,
+                    count: 1,
+                    values: [value],
+                    weightedMean: value,
+                    weightedDt: 0,
                 }
-            }
+            } else {
 
-            // Update the various fields of the record
-            acc[key].min = Math.min(acc[key].min, value);
-            acc[key].max = Math.max(acc[key].max, value);
-            acc[key].values.push(value);
-            acc[key].count++;
-            acc[key].mean += (value-acc[key].mean)/acc[key].count;
+                // Update the various fields of the record
+                acc[key].min = Math.min(acc[key].min, value);
+                acc[key].max = Math.max(acc[key].max, value);
+                acc[key].values.push(value);
+                acc[key].count++;
+                acc[key].mean += (value-acc[key].mean)/acc[key].count;
+
+                // Calculate time weighted mean
+                const dvi = (acc[key].lastValue + value) / 2;
+                const dti = timestamp.diff(acc[key].lastTimestamp).as('seconds');
+                
+                acc[key].weightedMean *= acc[key].weightedDt;
+                acc[key].weightedMean += dvi*dti;
+                acc[key].weightedDt += dti;
+                acc[key].weightedMean /= acc[key].weightedDt;
+
+                acc[key].lastValue = value;
+                acc[key].lastTimestamp = timestamp;
+            }
         }
+
         data = Object.values(acc)
 
-        // TODO: time weighted average
-        //
-        // let mean = Math.round(10* record.rows.weight.array().reduce((a,b) => a+b) / record.rows.length) / 10;
-        //
-        // let sum = 0;
-        // let test = record
-        //     .rows
-        //     .array()
-        // test.reduce((_a, _b, i, data) => {
-        //         sum += (data[i].timestamp.diff(data[i-1].timestamp))*(data[i-1].weight + data[i].weight)/2.0
-        //     })
-        // if (test.length > 1) {
-        //     mean = sum / (test[test.length-1].timestamp.diff(test[0].timestamp))
-        // }
-
-        const weightDataMax    = data.map(_ => ({'x': DateTime.fromMillis(_.key), 'y': _.max}))
-        const weightDataMean   = data.map(_ => ({'x': DateTime.fromMillis(_.key), 'y': _.mean}))
-        const weightDataMin    = data.map(_ => ({'x': DateTime.fromMillis(_.key), 'y': _.min}))
-
-        this.chart.data.datasets[0].data = weightDataMean;
-        this.chart.data.datasets[1].data = weightDataMax;
-        this.chart.data.datasets[2].data = weightDataMin;
+        this.chart.data.datasets[0].data = data.map(_ => ({'x': DateTime.fromMillis(_.key), 'y': _.max}));
+        this.chart.data.datasets[1].data = data.map(_ => ({'x': DateTime.fromMillis(_.key), 'y': _.weightedMean}));
+        this.chart.data.datasets[2].data = data.map(_ => ({'x': DateTime.fromMillis(_.key), 'y': _.min}));
         this.chart.update();
     }
 
 
     /**
-     *
-     **/
-    async onPanZoomComplete({chart}) {
-        let min = DateTime.fromMillis(chart.options.scales.x.min)
-        let max = DateTime.fromMillis(chart.options.scales.x.max)
-        const margin = Math.round(max.diff(min).as('days'));
-        this.loadData(min.minus({days: margin}), max.plus({days: margin}));
-    }
-
-    
-    /**
      * Callback function Obsidian invokes to render HTML.
      *
-     * This function is responsible to registering event listeners
-     * and using the Eta template to render the top level HTML.
+     * This function gets invoked by Obsidian each time a corresponding markdown
+     * code block with the correct "language" needs to be rendered. It is
+     * responsible for completely rendering the final look of the resulting
+     * HTML to show a line chart with historical weight data and an add button.
      **/
     async processMarkdown() {
 
+        // Reload weight data if the log file changes.
+        //
+        // https://docs.obsidian.md/Reference/TypeScript+API/Vault/on('modify')
+        //
+        // The "modify" event gets fired when any file in the vault is modified;
+        // calling the passed in function. The callback takes a single argument
+        // containing a reference to the file that was modified. We releat the
+        // data only if it matches the actual weight logfile.
+        this.app.vault.on('modify', (file:TAbstractFile) => (file == this.logFile) && this.loadData());
+
+        // Render the HTML contents of the container
+        //
+        // The HTML must include a <div> containing a <canvas> for the chart
+        // to be drawn on; chartjs requires that the <canvas> is the only child
+        // of its parent <div>.
+        //
+        // The HTML must also include a <div> containing the SVG for the add
+        // button. We use Eta's "include()" functionality to include the SVG
+        // file directly.
         this.container.innerHTML = this.eta.render("@view", {});
 
-        const button = this.container.querySelector('.add-button');
-        button.addEventListener("click", this.handleAddClick.bind(this));
+        // Register the click callback for the add button.
+        //
+        // Get a reference to the <div> that we are using as a button via its
+        // class; then register the callback frunction for the "click" event.
+        // The callback takes a single MouseEvent argument (which we ignore).
+        this.container
+            .querySelector('.add-button')
+            .addEventListener("click", this.handleAddClick.bind(this));
         
-        const ctx = this.container.querySelector('canvas');
-        this.chart = new Chart(ctx, {
-                  type: 'line',
-                  data: {
-                      datasets: [
-                          {data: []},
-                          {data: [], radius: 0, fill: '+1'},
-                          {data: [], radius: 0},
-                      ],
-                  },
-                  options: {
-                      animations: false,  // https://www.chartjs.org/docs/latest/configuration/animations.html
-                      elements: {         // https://www.chartjs.org/docs/3.9.1/configuration/elements.html
-                          line: {         // https://www.chartjs.org/docs/3.9.1/configuration/elements.html#line-configuration
-                              borderWidth: 1,
-                              tension: 0,  // zero means straight lines
-                              cubicInterpolationMode: 'monotone',
-                              borderColor: [
-                                  'rgb(75, 192, 192)',
-                                  'rgba(75, 192, 192, 0.10)',
-                                  'rgba(75, 192, 192, 0.10)',
-                              ],
-                              backgroundColor: [
-                                  'rgba(75, 192, 192, 0.10)',
-                                  'rgba(75, 192, 192, 0.10)',
-                                  'rgba(75, 192, 192, 0.10)',
-                              ],
-                          },
-                      },
-                      scales: {
-                          x: {
-                              type: 'time',
-                              min: DateTime.now().minus({weeks: 2}).toISO(),
-                              max: DateTime.now().toISO(),
-                              time: {minUnit: 'day'},
-                              ticks: {
-                                  minRotation: 60,
-                              },
-                              display: true,
-                          },
-                          y: {
-                              display: true,
-                              ticks: {
-                                  stepSize: 1,
-                              }
-                          }
-                      },
-                      plugins: {
-                          legend: {  // https://www.chartjs.org/docs/latest/configuration/legend.html
-                              display: false,
-                          },
-
-                          // https://www.chartjs.org/chartjs-plugin-zoom/guide/options.html
-                          zoom: {
-                              pan: {
-                                  enabled: true,
-                                  mode: 'x',
-                                  onPanComplete: this.onPanZoomComplete.bind(this),
-                              },
-                              zoom: {
-                                  wheel: {enabled: true, speed: 0.1, modifierKey: 'shift'},
-                                  pinch: {enabled: true, speed: 0.1},
-                                  mode: 'x',
-                                  onZoomComplete: this.onPanZoomComplete.bind(this),
-                              },
-                          },
-                      }
-                  },
+        // Define & configure a line chart for weight data
+        this.chart = new Chart(this.container.querySelector('canvas'), {
+            type: 'line',
+            data: {
+                datasets: [
+                    {label: 'max', radius: 0, fill: '+2'},
+                    {label: 'mean'},
+                    {label: 'min', radius: 0},
+                ],
+            },
+            options: {
+                animations: false,  // https://www.chartjs.org/docs/latest/configuration/animations.html
+                elements: {         // https://www.chartjs.org/docs/3.9.1/configuration/elements.html
+                    line: {         // https://www.chartjs.org/docs/3.9.1/configuration/elements.html#line-configuration
+                        borderWidth: 1,
+                        tension: 0,  // zero means straight lines
+                        cubicInterpolationMode: 'monotone',
+                        borderColor: [
+                            'rgba(75, 192, 192, 0.10)',  // max
+                            'rgb(75, 192, 192)',         // mean
+                            'rgba(75, 192, 192, 0.10)',  // min
+                        ],
+                        backgroundColor: [
+                            'rgba(75, 192, 192, 0.10)',  // max
+                            'rgba(75, 192, 192, 0.10)',  // mean
+                            'rgba(75, 192, 192, 0.10)',  // min
+                        ],
+                    },
+                },
+                scales: {
+                    x: {
+                        type: 'time',
+                        min: DateTime.now().minus({weeks: 2}).valueOf(),
+                        max: DateTime.now().valueOf(),
+                        time: {minUnit: 'day'},
+                        ticks: {minRotation: 60},
+                        display: true,
+                    },
+                    y: {
+                        display: true,
+                        ticks: { stepSize: 1 },                                        // only show whole pounds
+                        afterDataLimits: scale => { scale.max += 1, scale.min -= 1 },  // add an offset to axis min/max
+                    },
+                },
+                interaction: {mode: 'index'},  // https://www.chartjs.org/docs/latest/configuration/interactions.html#modes
+                plugins: {
+                    legend: {display: false},  // https://www.chartjs.org/docs/latest/configuration/legend.html
+                    zoom: {                    // https://www.chartjs.org/chartjs-plugin-zoom/guide/options.html
+                        pan: {
+                            enabled: true,
+                            mode: 'x',
+                            onPanComplete: _ => this.loadData(),
+                        },
+                        zoom: {
+                            wheel: {enabled: true, speed: 0.1, modifierKey: 'shift'},
+                            pinch: {enabled: true, speed: 0.1},
+                            mode: 'x',
+                            onZoomComplete: _ => this.loadData(),
+                        },
+                    },
+                    tooltip: {
+                        usePointStyle: true,
+                        callbacks: {  // https://www.chartjs.org/docs/latest/configuration/tooltip.html#tooltip-callbacks
+                            title: ctx => ctx[0].raw.x.toLocaleString(DateTime.DATE_MED),
+                            label: ctx => `${ctx.dataset.label}:\t${Math.round(10 * ctx.raw.y)/10}`,
+                            labelPointStyle: ctx => ({pointStyle: false}),
+                        },
+                    },
+                },
+            },
         });
 
-        this.loadData(DateTime.now().minus({weeks: 10}), DateTime.now().plus({days: 1}));
-        this.app.vault.on('modify', file => {
-            if (file = this.logFile) this.loadData(DateTime.now().minus({weeks: 2}), DateTime.now().plus({days: 1}));
-        });
+        // Load the data for the first time after Markdown rendering
+        this.loadData();
     }
 }
 
