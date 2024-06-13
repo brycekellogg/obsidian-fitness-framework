7105cbd7fd658ac5b9af870561184f6fc0b9128b
Bryce Kellogg <bryce@kellogg.org>
2024-06-13 15:02:55 -0700

    WIP: workout view

---
+++ src/templates/index.ts
@@ -0,0 +1,14 @@
+export { default as WorkoutView } from "inline:./workout.eta";
+export { default as WorkoutInProgress } from "inline:./workout-in-progress.eta";
+export { default as WorkoutNotStarted } from "inline:./workout-not-started.eta";
+export { default as WorkoutNext  } from "inline:./next.svg";
+export { default as WorkoutPrev  } from "inline:./prev.svg";
+export { default as WorkoutPlay  } from "inline:./play.svg";
+export { default as WorkoutStop  } from "inline:./stop.svg";
+export { default as WorkoutPause } from "inline:./pause.svg";
+export { default as WorkoutClose } from "inline:./close.svg";
+
+export { default as WeightView         } from "inline:./WeightView.eta";
+export { default as BloodPressureView  } from "inline:./BloodPressureView.eta";
+export { default as BloodPressureModal } from "inline:./BloodPressureModal.eta";
+export { default as Add } from "inline:./add.svg";
